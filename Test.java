f43f813f9428a5e573ef7db2758b22df1ba01dc5
guru <guru@gmail.com>
2026-03-05 08:31:01 +0530

    Modified

--- Test.java
+++ Test.java
@@ -1,8 +1,8 @@
 public class AdditionProgram {
     public static void main(String[] args) {
         // Declare and initialize two integer variables
-        int num1 = 25;
-        int num2 = 25;
+        int num1 = 100;
+        int num2 = 125;
         
         // Calculate the sum and store it in a third variable
         int sum = num1 + num2;
