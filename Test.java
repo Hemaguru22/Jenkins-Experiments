3f061385fa7c8e6c96ca5171844385619888da52
Hemaguru22 <hemagurup@gmail.com>
2026-03-05 08:11:33 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class AdditionProgram {
+    public static void main(String[] args) {
+        // Declare and initialize two integer variables
+        int num1 = 5;
+        int num2 = 15;
+        
+        // Calculate the sum and store it in a third variable
+        int sum = num1 + num2;
+        
+        // Print the result to the console
+        System.out.println("The sum is: " + sum);
+    }
+}
